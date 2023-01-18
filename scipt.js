4a26e1e4b6550b2ffd598cdfa1e923d10f28731d
Gabriel Lima <gabriel.lyma16@gmail.com>
2023-01-18 12:06:56 -0300

    aula 1

---
+++ scipt.js
@@ -0,0 +1,34 @@
+var primeiraVariavel = '10'; //texto / string// '' // //(nao recomendada em novos projetos)
+
+        let segundaVariavel = 10; //numero  ou inteiro// // (padrao atual)
+
+        const terceiraVariavel = '';
+        console.log(primeiraVariavel + segundaVariavel);
+
+        { //Supermercado
+            let cocaCola = 2.5
+            console.log('Supermercado', cocaCola);
+
+        }
+        { //lanchonete
+            let cocaCola = 5.5
+            console.log('lanchonete', cocaCola);
+        }
+
+
+        // let textoDigitado = prompt('digite alguma coisa usuario')
+        // console.log(textoDigitado)
+
+        //SOMAR DOIS NUMEROS INTEIROS
+
+        SOMA = N1 + N2
+        /* 1 - SOLICITAR AS ENTRADAS 
+         2 - ARMAZENAR AS ENTRADAS
+         3 - REALIZAR A SOMA 
+         4 - EXIBIR O RESULTADO
+        */
+        
+         let RecebePrimeiroNumero = Number(prompt('Digite o primeiro numero'));
+         let RecebeSegundoNumero = Number (prompt('Digite o segundo numero'));
+         let Soma = RecebeSegundoNumero + RecebePrimeiroNumero
+         console.log('Soma',Soma)
